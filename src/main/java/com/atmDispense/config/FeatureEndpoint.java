beaf0faa7e371bfb27ee9388ecf4c6a2a1371ff3
Srinivas1010 <thammisetty@gmail.com>
2021-08-25 04:46:34 +1000

    Update FeatureEndpoint.java

--- src/main/java/com/atmDispense/config/FeatureEndpoint.java
+++ src/main/java/com/atmDispense/config/FeatureEndpoint.java
@@ -15,29 +15,5 @@ import java.util.concurrent.ConcurrentHashMap;
 @Endpoint(id = "features")
 public class FeatureEndpoint {
 
-    private final Map<String, Feature> featureMap =
-            new ConcurrentHashMap<>();
-
-    public FeatureEndpoint() {
-        featureMap.put("Department", new Feature(true));
-        featureMap.put("User", new Feature(false));
-        featureMap.put("Authentication", new Feature(false));
-    }
-
-    @ReadOperation
-    public Map<String, Feature> features() {
-        return featureMap;
-    }
-
-    @ReadOperation
-    public Feature feature(@Selector String featureName) {
-        return featureMap.get(featureName);
-    }
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    private static class Feature {
-        private boolean isEnabled;
-    }
+   
 }
